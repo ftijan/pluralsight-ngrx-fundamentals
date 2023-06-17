309bbef3afab29e509d8f995128a4ac62bf90688
ftijan <41797259+ftijan@users.noreply.github.com>
2023-06-17 23:46:45 +0100

    ngrx install and example logic added (#4)
    * install ngrx package and initial use example

--- src/app/products/products-page/products-page.component.ts
+++ src/app/products/products-page/products-page.component.ts
@@ -3,6 +3,8 @@ import { sumProducts } from 'src/app/utils/sum-products';
 import { Product } from '../product.model';
 import { ProductsService } from '../products.service';
 
+import { Store } from '@ngrx/store';
+
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
@@ -12,10 +14,14 @@ export class ProductsPageComponent {
   products: Product[] = [];
   total = 0;
   loading = true;
-  showProductCode = false;
+  showProductCode$ = this.store.select(
+    (state: any) => state.products.showProductCode
+  );
   errorMessage = '';
 
-  constructor(private productsService: ProductsService) {}
+  constructor(private productsService: ProductsService, private store: Store) {
+    this.store.subscribe((store) => console.log(store));
+  }
 
   ngOnInit() {
     this.getProducts();
@@ -33,6 +39,6 @@ export class ProductsPageComponent {
   }
 
   toggleShowProductCode() {
-    this.showProductCode = !this.showProductCode;
+    this.store.dispatch({ type: '[Products Page] Toggle Show Product Code'});
   }
 }
