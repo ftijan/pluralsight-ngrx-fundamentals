5ac820d4a7bf90ab0549c8caf469f3b993670a28
ftijan <41797259+ftijan@users.noreply.github.com>
2023-06-18 20:59:01 +0100

    add component store use example (#11)

--- src/app/products/products-page/products-page.component.ts
+++ src/app/products/products-page/products-page.component.ts
@@ -1,24 +1,46 @@
-import { Component } from '@angular/core';
+import { 
+  Component, 
+// Uncomment for Product Store usage example:
+//  OnInit 
+} from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { ProductsPageActions } from '../state/products.actions';
 import { selectProducts, selectProductsErrorMessage, selectProductsLoading, selectProductsShowProductCode, selectProductsTotal } from '../state/products.selectors';
+// Uncomment for Product Store usage example:
+//import { ProductsStore } from '../state/products.store';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css'],
+  // Uncomment for Product Store usage example:
+  //providers: [ProductsStore]
 })
-export class ProductsPageComponent {
+export class ProductsPageComponent 
+// Uncomment for Product Store usage example:
+//implements OnInit 
+{
   products$ = this.store.select(selectProducts);
+  // Uncomment for Product Store usage example:
+  //componentStoreProducts$ = this.productsStore.products$;
   total$ = this.store.select(selectProductsTotal);
   loading$ = this.store.select(selectProductsLoading);
   showProductCode$ = this.store.select(selectProductsShowProductCode);
   errorMessage$ = this.store.select(selectProductsErrorMessage);
 
-  constructor(private store: Store) {
+  constructor(
+    private store: Store, 
+    // Uncomment for Product Store usage example:
+    //private productsStore: ProductsStore
+    ) {
     this.store.subscribe((store) => console.log(store));
-  }  
+  }
+
+  // Uncomment for Product Store usage example:
+  // ngOnInit(): void {
+  //   this.productsStore.getProducts();
+  // }
 
   toggleShowProductCode() {
     this.store.dispatch(ProductsPageActions.toggleShowProductCode());
